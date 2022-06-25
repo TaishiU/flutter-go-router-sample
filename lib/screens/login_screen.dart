cbde1a1b49093fca7e4ee1351ab7b1bb78f5f91b
Taishi U <nbadreamteam3@gmail.com>
2022-06-25 14:56:04 +0900

    UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,16 +8,112 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final formKey = GlobalKey<FormState>();
+    // late String _email;
+    // late String _password;
+    // bool _isObscure = true;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Screen'),
-      ),
-      body: const Center(
-        child: Text(
-          'Login',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 100),
+                const Text(
+                  'Welcome👋',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
+                ),
+                const SizedBox(height: 100),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                  ),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 20,
+                      ),
+                      hintText: 'Email',
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                    onChanged: (value) {
+                      // _email = value;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                  ),
+                  child: TextFormField(
+                    // obscureText: _isObscure,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 20,
+                      ),
+                      hintText: 'Password',
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        icon: const Icon(
+                          Icons.visibility_off,
+                          // _isObscure
+                          //     ? Icons.visibility_off
+                          //     : Icons.visibility,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    onChanged: (value) {
+                      // _password = value;
+                    },
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                ),
+                const SizedBox(height: 100),
+                SizedBox(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
